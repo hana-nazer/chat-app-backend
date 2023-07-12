398261d0e7c5097600b4bfcf9ba70f5f30d8692f
hana-nazer <hanapp9966@gmail.com>
2023-07-12 15:08:14 +0530

    Add authentication

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
+const connectDB = require("./config/db");
+dotenv.config();
+connectDB();
 
+app.use(express.json()); // to accept json data
+app.use("/api/user", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
-app.get("/api/chat", (req, res) => {
-  res.send("running");
-});
-
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log("server started");
